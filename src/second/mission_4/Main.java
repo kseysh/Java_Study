01e833c703acc6886c8cf67cf93918873d3d425a
김승환 <kseysh123@naver.com>
2023-02-18 21:15:07 +0900

    intellij commit 2

--- src/second/mission_4/Main.java
+++ src/second/mission_4/Main.java
@@ -1,16 +1,20 @@
 package second.mission_4;
 
 public class Main {
-    int a = 7;
-    int b = 5;
-    int c = 3;
-    int d = 6;
-    //System.out.println(++a); //8
-    //System.out.println(b++); //5
-    //System.out.println(c--); //3
-    //System.out.println(--d); //5
-    //System.out.println(a); //8
-    //System.out.println(b); //6
-    //System.out.println(c); //2
-    //System.out.println(d); //5
+
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 5;
+        int c = 3;
+        int d = 6;
+        System.out.println(++a); //8
+        System.out.println(b++); //5
+        System.out.println(c--); //3
+        System.out.println(--d); //5
+        System.out.println(a); //8
+        System.out.println(b); //6
+        System.out.println(c); //2
+        System.out.println(d); //5
+    }
+
 }
